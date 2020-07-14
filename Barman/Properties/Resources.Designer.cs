ef5cd577ef2e1532b31ea0869703e3639c51d036
Daniel Shultz <Dani666dani@mail.ru>
2020-07-14 20:27:46 +0300

    Name fix

--- Barman/Properties/Resources.Designer.cs
+++ Barman/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Tourism.Properties {
+namespace Barman.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace Tourism.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Tourism.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Barman.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
